30a717d682529417e446d6560de24feb07a8953d
Seres Ákos <akosseres75@gmail.com>
2024-03-09 13:52:35 +0100

    init

--- src/web_app/environments/dev/common/config/main-local.php
+++ src/web_app/environments/dev/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=db;port=3306;dbname=webshop',
             'username' => 'root',
-            'password' => '',
+            'password' => 'password',
             'charset' => 'utf8',
         ],
         'mailer' => [
