34102a0522eab7fb94bf1fc3e620341a8c8e2302
Seres Ákos <akosseres75@gmail.com>
2024-04-15 14:43:56 +0200

    fixing alert bug in wrapper

--- src/web_app/frontend/views/user/account-wrapper.php
+++ src/web_app/frontend/views/user/account-wrapper.php
@@ -53,6 +53,18 @@ $link = $tabs[$tab]['link'];
 
 $this->title = ucfirst($tab);
 
+foreach (Yii::$app->session->getAllFlashes() as $key => $message):
+    if(str_contains($key, 'Success')): ?>
+        <div class="alert alert-success  my-3">
+            <?= $message ?>
+        </div>
+    <?php elseif (str_contains($key, 'Error')): ?>
+        <div class="alert alert-danger my-3">
+            <?= $message ?>
+        </div>
+    <?php endif;
+endforeach;
+
 echo Navigation::widget([
     'tabs' => $tabs,
     'tab' => $tab
