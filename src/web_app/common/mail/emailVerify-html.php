852603074bb1fd4a3249ef8a3c2530291d7e7512
Seres Ákos <akosseres75@gmail.com>
2024-04-20 13:04:01 +0200

    updating email verification html

--- src/web_app/common/mail/emailVerify-html.php
+++ src/web_app/common/mail/emailVerify-html.php
@@ -12,74 +12,19 @@ use yii\web\View;
 $verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'token' => $user->verification_token]);
 ?>
 
-<style>
-    body {
-        background-color: #fe9003;
-    }
-    h1 {
-        font-size: 26px;
-    }
-
-    h5 {
-        font-size: 20px;
-    }
-
-    .d-flex {
-        display: flex;
-    }
-
-    .site-signup {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .container {
-        padding: 20px 40px;
-        display: flex;
-        flex-direction: column;
-        background-color: white;
-        border-radius: 20px;
-    }
-
-    .text-center {
-        text-align: center;
-    }
-
-    .btn {
-        padding: 15px 30px;
-        text-decoration: none;
-        border-radius: 5px;
-        color: black;
-        background-color: transparent;
-        transition: all 0.15s ease-in-out;
-    }
-
-    .btn.btn-outline-dark {
-        border: 1px solid black;
-    }
-
-    .btn.btn-outline-dark:hover {
-        background-color: black;
-        color: white;
-    }
-
-    .link-span {
-        justify-content: center;
-    }
-</style>
-
-<div class="site-signup">
-    <div class="container">
-        <h1 class="text-center">
+<div class="container-fluid mt-5">
+    <div class="container d-flex flex-column align-items-center justify-content-center">
+        <h1 class="row mb-3">
             Welcome To Sportify <?= $user->username ?>!
         </h1>
-        <h5 class="text-center">
-            Please Verify Your Email to be able to use your account!
+        <h5 class="row mb-3">
+            Please verify your email to be able to use your account!
         </h5>
-        <span class="d-flex link-span text-center">
-            <a class="btn btn-outline-dark" href="<?= Html::encode($verifyLink)?>">VERIFY EMAIL</a>
-        </span>
+        <div class="row">
+            <div class="col">
+                <a class="btn btn-outline-dark" href="<?= Html::encode($verifyLink)?>">VERIFY EMAIL</a>
+            </div>
+        </div>
     </div>
 </div>
 
--- src/web_app/common/mail/layouts/html.php
+++ src/web_app/common/mail/layouts/html.php
@@ -17,6 +17,7 @@ use yii\web\View;
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
     <title><?= Html::encode($this->title) ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <?php $this->head() ?>
 </head>
 <body>
