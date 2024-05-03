e05330c1f82d764d9cb7e5b8ca18d856d52c35b1
akosseres09 <101422253+akosseres09@users.noreply.github.com>
2024-05-03 19:26:22 +0200

    fixing deleted code

--- src/web_app/backend/web/js/app.js
+++ src/web_app/backend/web/js/app.js
@@ -28,3 +28,30 @@ if (editUserModal && editUserBtn){
        });
     });
 }
+
+function triggerFileSelect() {
+    document.querySelector('input[type="file"]').click();
+}
+
+function showFiles(event) {
+    const files = event.target.files;
+    const container = document.getElementById('uploadedImagesContainer');
+    container.innerHTML = '';
+    const gap = files.length * 10;
+    const padding = 2 * 24 * 0.5;
+    const width = container.clientWidth - gap - padding;
+
+    for (const file of files) {
+        const reader = new FileReader();
+        reader.onloadend = function (e) {
+            const img = document.createElement('img');
+            img.src = e.target.result;
+            img.alt = file.name;
+            img.style.minWidth = width/6 + 'px';
+            img.style.width = width / files.length + 'px';
+            img.style.height = 'auto';
+            container.appendChild(img);
+        }
+        reader.readAsDataURL(file);
+    }
+}
